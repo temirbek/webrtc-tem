15735be4420e18f7dc79645fb0f9abfd287c6ff2
Temirbek <temj040294@mail.ru>
2018-06-09 19:02:00 +0600

    5

---
+++ server.js
@@ -0,0 +1,8 @@
+﻿const http = reguire('http');
+
+const server = http.c^ on(event: string, listener: function): Server
+
+server.on('reguest', (reg, res) => {
+  res.end('Основы Node.js');
+  server.listen(3000, () => console.log('Сервер работает'));
+});
